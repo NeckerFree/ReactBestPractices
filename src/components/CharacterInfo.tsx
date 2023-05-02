acf3e050c30a9d235e0da0225bc7c9a387a9dace
Elio Cortés <elio.cortes3000@gmail.com>
2023-05-01 21:39:10 -0500

    Error control V1

---
+++ src/components/CharacterInfo.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+interface Props {
+  characterId: number;
+}
+
+const CharacterInfo: React.FC<Props> = ({ characterId }) => {
+  const [character, setCharacter] = useState<Character | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchCharacter = async () => {
+    try {
+      const response = await axios.get<Character>(
+        `https://rickandmortyapi.com/api/characte/${characterId}`
+      );
+      setCharacter(response.data);
+      setError(null);
+    } catch (e) {
+      setCharacter(null);
+      setError(e.response.data.error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={fetchCharacter}>Fetch Character</button>
+      {error && <p>{error}</p>}
+      {character && (
+        <div>
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+          <p>Status: {character.status}</p>
+          <p>Species: {character.species}</p>
+          <p>Type: {character.type}</p>
+          <p>Gender: {character.gender}</p>
+          <p>Origin: {character.origin.name}</p>
+          <p>Location: {character.location.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CharacterInfo;
